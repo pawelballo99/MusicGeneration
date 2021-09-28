73039d35ac3eff16284149f19407a4ae494d1a38
pawelballo@gmail.com <Polak2012>
2021-09-28 21:29:57 +0200

    extracting midies

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
+import collections
+from urllib.request import urlopen
+from io import BytesIO
 import pretty_midi
+from zipfile import ZipFile
+import datetime
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import random
+import tensorflow as tf
 import os
 
-def monophonic(stream):
-    try:
-        length = len(instrument.partitionByInstrument(stream).parts)
-    except:
-        length = 0
-    return length == 1
+SIZE_DATASET = 10
 
-if __name__=="__main__":
-    data_dir = os.path.abspath(os.getcwd())+ '\\midi\\'
-    songList = os.listdir(data_dir)
-    originalScores = []
-    for song in songList:
-        midi_pretty_format = pretty_midi.PrettyMIDI(os.getcwd()+ '\\midi\\' + song)
-        piano_midi = midi_pretty_format.instruments[0] # Get the piano channels
-        piano_roll = piano_midi.get_piano_roll(fs=5)
-        #score = converter.parse(data_dir+song)
-        """ if(monophonic(score)):
-            originalScores.append(score.chordify())
-            print(str(len(originalScores))) """
 
-    """ originalChords = [[] for _ in originalScores]
-    originalDurations = [[] for _ in originalScores]
-    originalKeys = []
+def midis_to_notes(midi_file: str) -> [pd.DataFrame]:
+    dataframes_midis = []
+    for file in midi_file[:SIZE_DATASET]:
+        pm = pretty_midi.PrettyMIDI(file)
+        instrument = pm.instruments[0]
+        notes = collections.defaultdict(list)
 
-    for i, song in enumerate(originalScores):
-        originalKeys.append(str(song.analyze('key')))
-        for element in song:
-            originalChords[i].append('.'.join(str(n) for n in element.pitches))
-            originalDurations[i].append(element.duration.quarterLength)
-        print(str(i)) """
+        sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+        prev_start = sorted_notes[0].start
+        for note in sorted_notes:
+            start = note.start
+            end = note.end
+            notes['pitch'].append(pretty_midi.note_number_to_name(note.pitch))
+            notes['start'].append(start)
+            notes['end'].append(end)
+            notes['step'].append(start - prev_start)
+            notes['duration'].append(end - start)
+            prev_start = start
 
+        dataframes_midis.append(pd.DataFrame({name: np.array(value) for name, value in notes.items()}))
+        print(len(dataframes_midis))
+    return dataframes_midis
+
+
+def download_MAESTRO():
+    data_dir = pathlib.Path('midi/maestro-v3.0.0')
+    if not data_dir.exists():
+        http_response = urlopen("https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0"
+                                "-midi.zip")
+        zipfile = ZipFile(BytesIO(http_response.read()))
+        zipfile.extractall(path='midi/')
+    filenames = glob.glob(str(data_dir / '**/*.mid*'))
+    random.shuffle(filenames)
+    print('Number of files:', len(filenames))
+    return filenames
+
+
+if __name__ == "__main__":
+    midis = download_MAESTRO()
+    x = midis_to_notes(midis)
     print('kkk')
