e4afca6c48987f85e9085d1639daca6f25ca8746
pawelballo@gmail.com <Polak2012>
2021-12-16 19:28:57 +0100

    tensorflow templete

--- generate.py
+++ generate.py
@@ -1,18 +1,23 @@
-from main import *
 import numpy as np
+import os
+import pathlib
+import pretty_midi
+from PIL import Image
+from model.model import Model
+from utils.config import process_config
 
-length_song = 100
+length_song = 50
 
 
-def generate_song(array_song):
+def generate_song(array_song, fps):
     array_song = array_song.T
     new_midi = pretty_midi.PrettyMIDI()
     midi_list = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Acoustic Grand Piano'))
 
-    data = im.fromarray(array_song * 255)
-    if data.mode != 'L':
-        data = data.convert('L')
-    data.save('generated_song.png')
+    # data = im.fromarray(array_song * 255)
+    # if data.mode != 'L':
+    #     data = data.convert('L')
+    # data.save('generated_song.png')
 
     print('\n------ Writing MIDI ------\n')
     for i in range(array_song.shape[0]):
@@ -25,38 +30,50 @@ def generate_song(array_song):
                     print("pitch:" + str(i))
                     print("time:" + str(tm) + "\n")
                     note = pretty_midi.Note(
-                        velocity=100, pitch=i, start=(j - tm) / FPS, end=j / FPS)
+                        velocity=100, pitch=i, start=(j - tm) / fps, end=j / fps)
                     midi_list.notes.append(note)
                     tm = 0
     new_midi.instruments.append(midi_list)
-    print('\n------ Saving MIDI ------\n')
-    new_midi.write('generated_song.midi')
+    dir="generated_midi\\"
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    new_midi.write(dir+'generated_song.midi')
+    data = Image.fromarray(array_song * 255)
+    if data.mode != 'L':
+        data = data.convert('L')
+    data.save(dir+'generated_song.png')
+
+def get_path():
+    best_h5 = 0
+
+    dir = "experiments\\gen1\\checkpoint"
+    for subdir, dirs, files in os.walk(dir):
+        for file in files:
+            value=int(file.split("_")[3].split('.')[1])
+            if best_h5<value:
+                best_h5=value
+                idx=files.index(file)
+    return dir+"\\"+files[idx]
+
+
 
 
 if __name__ == "__main__":
     #start_frame = download_MAESTRO(15)[9][:SEQ_SIZE]
-    start_frame= np.random.randint(0,2,size=(SEQ_SIZE,128))
+    config = process_config()
+    model = Model(config).model
+    path = get_path()
+    model.load_weights(path)
+    start_frame = np.random.randint(0, 2, size=(config.data_loader.seq_size, 128))
+    vanish_proof = 0.2
+    vanish_inc = 1.001
+    for i in range(length_song * config.data_loader.fps):
+        y = model.predict(np.expand_dims(start_frame[i:config.data_loader.seq_size + i, :], axis=[0, 1]))+vanish_proof
+        for i in range(y.shape[1]):
+            if y[0, i] >= 0.5:
+                y[0, i] = 1
+            else:
+                y[0, i] = 0
+        start_frame = np.concatenate((start_frame, y), axis=0)
 
-    print('\n------ Creating model ------\n')
-    model = get_model()
-
-    if path.isfile("best_model.hdf5"):
-        print('\n------ Loading weights ------\n')
-        model.load_weights("best_model.hdf5")
-
-        print('\n------ Generating MIDI ------\n')
-        vanish_proof = 0.2
-        vanish_inc = 1.001
-        for i in range(length_song * FPS):
-            y = model.predict(np.expand_dims(start_frame[i:SEQ_SIZE + i, :], axis=[0, 1])) + 0.12
-            for i in range(y.shape[1]):
-                if y[0, i] >= 0.5:
-                    y[0, i] = 1
-                else:
-                    y[0, i] = 0
-            start_frame = np.concatenate((start_frame, y), axis=0)
-
-        generate_song(start_frame)
-    else:
-        print('\nTrain model first!!!\n')
-    print('F')
+    generate_song(start_frame, config.data_loader.fps)
--- main.py
+++ main.py
@@ -1,123 +1,23 @@
+from utils.config import process_config
+from model.model import Model
+from data_loader.data_loader import DataLoader
+from trainer.trainer import ModelTrainer
+import sys
 import os
-import random
-from os import path
-from urllib.request import urlopen
-from io import BytesIO
-import pretty_midi
-from zipfile import ZipFile
-import glob
-import numpy as np
-import pathlib
-from time import time
-from PIL import Image as im
-import sklearn
-from sklearn.preprocessing import MultiLabelBinarizer
-from tensorflow import clip_by_value
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten, \
-    BatchNormalization, Input, ConvLSTM2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
-from tensorflow.python.keras.backend import _to_tensor
-from tensorflow.python.keras.backend_config import epsilon
-from tensorflow.python.keras.layers import TimeDistributed, Conv1D, MaxPooling1D, LSTM
-from tensorflow.python.keras.losses import BinaryCrossentropy
-from tensorflow.python.layers.pooling import MaxPooling3D, MaxPooling2D
-from tensorflow.python.ops.nn_impl import sigmoid_cross_entropy_with_logits
-from tensorflow.python.ops.numpy_ops import log
-
-SIZE_DATASET = 50
-FPS = 10
-SEQ_SIZE = 80
-
-random.seed(50)
+import h5py
 
 
-def download_MAESTRO(dataset):
-    data_dir = pathlib.Path('midi/maestro-v3.0.0')
-    if not data_dir.exists():
-        http_response = urlopen("https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0"
-                                "-midi.zip")
-        zipfile = ZipFile(BytesIO(http_response.read()))
-        zipfile.extractall(path='midi/')
-    filenames = glob.glob(str(data_dir / '**/*.mid*'))
-    piano_rolls = []
-    for file in filenames[:dataset]:
-        mid = pretty_midi.PrettyMIDI(file)
-        frame = np.transpose(mid.get_piano_roll(fs=FPS)).astype(bool).astype(float)
-        piano_rolls.append(frame)
-        print('Number of songs:', len(piano_rolls))
-    return piano_rolls
+def main():
+    try:
+        config = process_config()
+        data = DataLoader(config)
+        model = Model(config)
+        trainer = ModelTrainer(model, data, config)
+        trainer.train()
+
+    except Exception as e:
+        print(e)
 
 
-def get_train_and_target(piano_rolls):
-    x_train = []
-    y_target = []
-    piano_rolls = np.vstack(piano_rolls)
-
-    data = im.fromarray(piano_rolls[:1024][:]*255)
-    if data.mode != 'L':
-        data = data.convert('L')
-    data.save('data_vizualization.png')
-
-    for i in range(piano_rolls.shape[0] - SEQ_SIZE):
-        x_train.append(piano_rolls[i:SEQ_SIZE + i, :])
-        y_target.append(piano_rolls[SEQ_SIZE + i, :])
-    return np.asarray(x_train), np.asarray(y_target)
-
-
-def bc(target, output, from_logits=False):
-    if not from_logits:
-        _epsilon = _to_tensor(epsilon(), output.dtype.base_dtype)
-        output = clip_by_value(output, _epsilon, 1 - _epsilon)
-        output = log(output / (1 - output))
-
-    return sigmoid_cross_entropy_with_logits(labels=target,
-                                             logits=output)
-
-
-def get_model():
-    model = Sequential()
-    model.add(TimeDistributed(Conv1D(filters=135, kernel_size=2, activation='relu'),
-                              input_shape=(None, SEQ_SIZE, 128)))
-    model.add(TimeDistributed(MaxPooling1D(pool_size=1, strides=None)))
-    model.add(TimeDistributed(Conv1D(filters=135, kernel_size=2, activation='relu')))
-    model.add(TimeDistributed(MaxPooling1D(pool_size=1, strides=None)))
-    model.add(TimeDistributed(Conv1D(filters=135, kernel_size=2, activation='relu')))
-    model.add(TimeDistributed(MaxPooling1D(pool_size=1, strides=None)))
-    model.add(TimeDistributed(Flatten()))
-    model.add(LSTM(256, return_sequences=True))
-    model.add(LSTM(128))
-    model.add(BatchNormalization())
-    model.add(Dense(128, activation='sigmoid'))
-    model.compile(optimizer='RMSprop', loss=bc, metrics=['accuracy'], )
-    model.summary()
-
-    return model
-
-
-if __name__ == "__main__":
-    x = download_MAESTRO(SIZE_DATASET)
-    train, target = get_train_and_target(x)
-    train = np.expand_dims(train, axis=1)
-
-    print('\n------ Finished preprocessing ------\n')
-    mlb = MultiLabelBinarizer()
-    mlb.fit(target)
-
-    model = get_model()
-
-    print('\n------ Model created ------\n')
-    if path.isfile("best_model.hdf5"):
-        try:
-            model.load_weights("best_model.hdf5")
-            print('\n------ Weights loaded ------\n')
-        except:
-            os.remove("best_model.hdf5")
-            pass
-
-    checkpoint = ModelCheckpoint("best_model.hdf5", monitor='accuracy', verbose=1,
-                                 save_best_only=True, mode='auto', period=1)
-
-    print('\n------ Start training ------\n')
-    model.fit(train, target, epochs=150, verbose=1, callbacks=[checkpoint])
+if __name__ == '__main__':
+    main()
