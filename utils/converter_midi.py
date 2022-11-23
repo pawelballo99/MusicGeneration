3aa66398c14a4201b0c5e402d35826bdcef400e0
Paweł <pawelballo@gmail.com>
2022-11-23 12:51:29 +0100

    converter changes

--- utils/converter_midi.py
+++ utils/converter_midi.py
@@ -10,38 +10,42 @@ from config import process_config
 config = process_config()
 
 majors = dict(
-    [("A-", 4), ("A", 3), ("B-", 2), ("B", 1), ("C", 0), ("D-", -1), ("D", -2), ("E-", -3), ("E", -4), ("F", -5),
-     ("G-", 6), ("G", 5)])
+    [("A-", 4), ("G#", 4), ("A", 3), ("A#", 2), ("B-", 2), ("B", 1), ("C", 0), ("C#", -1), ("D-", -1), ("D", -2),
+     ("D#", -3), ("E-", -3), ("E", -4), ("F", -5), ("F#", 6), ("G-", 6), ("G", 5)])
 minors = dict(
-    [("A-", 1), ("A", 0), ("B-", -1), ("B", -2), ("C", -3), ("D-", -4), ("D", -5), ("E-", 6), ("E", 5), ("F", 4),
-     ("G-", 3), ("G", 2)])
+    [("G#", 1), ("A-", 1), ("A", 0), ("A#", -1), ("B-", -1), ("B", -2), ("C", -3), ("C#", -4), ("D-", -4), ("D", -5),
+     ("D#", 6), ("E-", 6), ("E", 5), ("F", 4), ("F#", 3), ("G-", 3), ("G", 2)])
 
 
-def midi_to_png(file):
-    score = music21.converter.parse(file)
-    key1 = score.analyze('key')
-    if not (key1.tonic.name == "C" and key1.mode == "major") and not (key1.tonic.name == "A" and key1.mode == "minor"):
-        try:
-            if key1.mode == "major":
-                halfSteps = majors[key1.tonic.name]
-            else:
-                halfSteps = minors[key1.tonic.name]
-            os.remove(file)
-            score.transpose(halfSteps).write('midi', file)
-            mid = pretty_midi.PrettyMIDI(os.path.join(r, file))
-            frame = (mid.get_piano_roll(fs=8)).astype(bool).astype(float)
-            save_png(frame)
-        except:
-            print(str(file) + " was removed")
-            if os.path.exists(file):
-                os.remove(file)
-    else:
-        mid = pretty_midi.PrettyMIDI(os.path.join(r, file))
-        frame = (mid.get_piano_roll(fs=config.data_loader.fps)).astype(bool).astype(float)
-        save_png(frame)
+def midi_to_png(midi_file):
+    # score = music21.converter.parse(midi_file)
+    # key1 = score.analyze('key')
+    # if not (key1.tonic.name == "C" and key1.mode == "major") and not (key1.tonic.name == "A" and key1.mode == "minor"):
+    #     try:
+    #         if key1.mode == "major":
+    #             half_steps = majors[key1.tonic.name]
+    #         else:
+    #             half_steps = minors[key1.tonic.name]
+    #         new_score = score.transpose(half_steps)
+    #         os.remove(midi_file)
+    #         new_score.write('midi', midi_file)
+    #         print(str(midi_file) + " was converted from " + key1.tonic.name + " " + key1.mode)
+    #         mid = pretty_midi.PrettyMIDI(os.path.join(r, midi_file))
+    #         frame = (mid.get_piano_roll(fs=config.data_loader.fps)).astype(bool).astype(float)
+    #         save_png(frame)
+    #     except Exception as e:
+    #         print(str(midi_file) + " was removed with exeception " + str(e))
+    #         if os.path.exists(midi_file):
+    #             os.remove(midi_file)
+    # else:
+    mid = pretty_midi.PrettyMIDI(os.path.join(r, midi_file))
+    frame = (mid.get_piano_roll(fs=config.data_loader.fps)).astype(bool).astype(float)
+    save_midi_as_png(frame)
 
 
-def save_png(frame):
+def save_midi_as_png(midi_file):
+    mid = pretty_midi.PrettyMIDI(os.path.join(r, midi_file))
+    frame = (mid.get_piano_roll(fs=config.data_loader.fps)).astype(bool).astype(float)
     for j in range(frame.shape[0] - config.data_loader.seq_size):
         img_x = im.fromarray(frame[:, j:config.data_loader.seq_size + j] * 255)
         img_y = im.fromarray(frame[:, config.data_loader.seq_size + j] * 255)
@@ -73,4 +77,4 @@ if __name__ == "__main__":
         for file in f:
             filenames.append(os.path.join(r, file))
     for i in tqdm(range(len(filenames))):
-        midi_to_png(filenames[i])
+        save_midi_as_png(filenames[i])
